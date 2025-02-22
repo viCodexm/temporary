d226fff5e9f1bcb5faf9c71072672161f84d6a75
Юра Коновалов <138516674+YourCheckGit@users.noreply.github.com>
2025-02-22 13:04:59 +0300

    changed main (yura)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-  std::cout << "------------------\n";
-  std::cout << "This is my code!\n";
-  std::cout << "I love coding!\n";
-  std::cout << "------------------\n";
+  std::cout << "++++++++++++++++++\n";
+  std::cout << "NOOO, this is my code!\n";
+  std::cout << "I love copybara!\n";
+  std::cout << "++++++++++++++++++\n";
   return 0;
 }
